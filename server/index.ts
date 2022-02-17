58879408af383be5618ae41ffe41d7111f8d7272
Kenneth Bakke <kennethsbakke@gmail.com>
2022-02-16 22:07:27 -0800

    Added middleware to serve build version of react app

--- server/index.ts
+++ server/index.ts
@@ -1,5 +1,6 @@
-import express, { Response, Request } from 'express';
+import express from 'express';
 import * as dotenv from 'dotenv';
+import path from 'path';
 import cors from 'cors';
 import router from './routes/index';
 
@@ -16,6 +17,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, '../client/build/')));
 app.use('/', router);
 
 app.listen(PORT, () => {
